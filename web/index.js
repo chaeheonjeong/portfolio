6af7a867ab47bcd11250c16b5dd74c3e00ed5272
chaeheonjeong <cogjs3210@naver.com>
2024-01-18 13:55:13 +0900

    feat: Add nav, contact email

---
+++ web/index.js
@@ -0,0 +1,12 @@
+document.querySelector("#navHome").addEventListener("click", (e) => {
+  document.querySelector("#home").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector("#navProject").addEventListener("click", (e) => {
+  document.querySelector("#project").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector("#navRecommend").addEventListener("click", (e) => {
+  document.querySelector("#recommend").scrollIntoView({ behavior: "smooth" });
+});
+document.querySelector("#navContact").addEventListener("click", (e) => {
+  document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
+});
